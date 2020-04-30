c1dbae1794de8134e7bed1d3cb2caaa4bf712206
davide cardia <davidecardiawd@gmail.com>
2020-04-30 12:38:39 +0200

    first commit

---
+++ app/components/Header/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import NavBar from './NavBar';
+import HeaderLink from './HeaderLink';
+
+import messages from './messages';
+
+function Header() {
+  return (
+    <div>
+      <NavBar>
+        <HeaderLink to="/">
+          <FormattedMessage {...messages.home} />
+        </HeaderLink>
+      </NavBar>
+    </div>
+  );
+}
+
+export default Header;
