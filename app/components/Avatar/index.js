18e91e35a2c11aea36fc8733b6ae6249cf7d4946
davide cardia <davidecardiawd@gmail.com>
2020-05-17 19:50:31 +0200

    profile added

--- app/components/Avatar/index.js
+++ app/components/Avatar/index.js
@@ -3,17 +3,22 @@ import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-    },
-  }));
+  root: {
+  },
+  large: {
+    width:  props => `${props.size}px`,
+    height: props => `${props.size}px`,
+  }
+}));
 
 const ImageAvatar = (props) => {
-    const classes = useStyles();
-    return (
-      <div className={classes.root}>
-        <Avatar src={props.src} />
-      </div>
-    );
+
+  const classes = useStyles(props);
+  return (
+    <div className={classes.root}>
+      <Avatar src={props.src} size={props.size} className={classes.large} />
+    </div>
+  );
 }
 
 export default ImageAvatar;
--- app/containers/Profile/index.js
+++ app/containers/Profile/index.js
@@ -15,40 +15,41 @@ import { compose } from "redux";
 import { useInjectSaga } from "utils/injectSaga";
 import { useInjectReducer } from "utils/injectReducer";
 import makeSelectProfile from "./selectors";
+import { makeSelectUserData } from 'containers/App/selectors';
+
 import reducer from "./reducer";
 import saga from "./saga";
 import messages from "./messages";
 
 import FadeIn from 'components/FadeIn';
 import Wrapper from 'components/Wrapper';
+import ImageAvatar from 'components/Avatar';
 
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import { Typography } from "@material-ui/core";
 
-export function Profile() {
+export function Profile({ userData }) {
   useInjectReducer({ key: "profile", reducer });
   // useInjectSaga({ key: "profile", saga });
-
+  const { photo } = userData;
   return (
     <FadeIn>
-      <div>
-        <Grid container justify="center" alignItems="center" style={{ height: '90vh' }}>
-          <Grid item xs={4} >
-            <Wrapper flex direction="column">
-
-              <Typography variant="h1" gutterBottom >Hello!</Typography>
-              <Typography variant="h6" gutterBottom >Inserisci i tuoi dati personali e ciaone</Typography>
-            </Wrapper>
-          </Grid>
-          {/* <Grid item xs={4} >
+      <Grid container justify="center" alignItems="center" style={{ height: '90vh' }}>
+        <Grid item xs={4} >
+          <Wrapper flex direction="column">
+            <ImageAvatar src={photo} size={200} />
+            <Typography variant="h1" gutterBottom >Hello!</Typography>
+            <Typography variant="h6" gutterBottom >Inserisci i tuoi dati personali e ciaone</Typography>
+          </Wrapper>
+        </Grid>
+        {/* <Grid item xs={4} >
             <Wrapper flex direction="column">
               <Typography variant="h1" gutterBottom >Hello!</Typography>
               <Typography variant="h6" gutterBottom >Inserisci i tuoi dati personali e ciaone</Typography>
             </Wrapper>
           </Grid> */}
-        </Grid>
-      </div>
+      </Grid>
     </FadeIn>
 
   );
@@ -59,8 +60,10 @@ Profile.propTypes = {
 };
 
 const mapStateToProps = createStructuredSelector({
-  profile: makeSelectProfile()
+  profile: makeSelectProfile(),
+  userData: makeSelectUserData(),
 });
+
 
 function mapDispatchToProps(dispatch) {
   return {
